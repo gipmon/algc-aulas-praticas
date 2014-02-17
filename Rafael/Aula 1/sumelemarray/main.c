522c55e3f3054e6b687bde1dd08b781f1c4f2490
Rafael Ferreira <mail@rafaelferreira.pt>
2014-02-17 18:31:38 +0000

    teste

--- Rafael/Aula 1/sumelemarray/main.c
+++ Rafael/Aula 1/sumelemarray/main.c
@@ -8,12 +8,13 @@ int count = 0;
 
 int main(void){
 	unsigned int array[] = {1, 2, 3, 4};
-	printf("Sumatório: %2d\n", sumElemInArray(array, 4));
+	unsigned int result = sumElemInArray(array, 4);
+	printf("Sumatório: %2d\nOperações: %2d\n", result, count);
 }
 
 unsigned int sumElemInArray(unsigned int a[], int length){
 	unsigned int sum = 0, i;
-	for(i = 0; i<length; i++){
+	for(i = 0; i<length; i++, count++){
 		sum += a[i];
 	}
 	return sum;
