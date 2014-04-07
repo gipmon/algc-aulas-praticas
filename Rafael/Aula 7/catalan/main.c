29054f6e47e35e9849b850e170113428c451cf62
Rafael Ferreira <mail@rafaelferreira.pt>
2014-04-07 15:10:53 +0100

    catalan e somatorio

---
+++ Rafael/Aula 7/catalan/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int nMults;
+int catalanRecursive(unsigned int);
+int catalanDinamic(unsigned int);
+
+int main(void){
+    int n,i;
+
+    do{
+        printf("Insira o valor de n: ");
+        scanf("%d", &n);
+    }while(n<0);
+
+    printf("\n");
+    printf(" %-5s |%-8s |%-14s |%-14s \n","n","Catalan (n)","M(n) Rec","M(n) Din");
+    printf("------------------------------------------------\n");
+
+    for (i=0;i<=n;i++){
+        nMults = 0;
+        printf(" %-6d| %-11d", i, catalanRecursive(i));
+        printf("| %-14d", nMults);
+
+        nMults = 0;
+        catalanDinamic(i);
+        printf("| %-14d\n",nMults);
+    }
+
+    return 0;
+}
+
+int catalanRecursive(unsigned int n){
+    int i;
+    int sum = 0;
+
+    if (n == 0) {
+        return 1;
+    }else{
+        for(i = 0; i < n; i++){
+            nMults++;
+            sum += catalanRecursive(i) * catalanRecursive(n - i - 1);
+        }
+    }
+
+    return sum;
+}
+
+int catalanDinamic(unsigned int n){
+    int i, j;
+    int sum;
+    int *array = ( int*) malloc(n * sizeof( int));
+
+    array[0] = 1;
+    for(i = 1; i <= n; i++){
+        sum = 0;
+        for(j = 0; j < i; j++){
+            nMults++;
+            sum += array[j] * array[i - j - 1];
+        }
+        array[i] = sum;
+    }
+    return array[n];
+}
--- Rafael/Aula 7/somatorio/main.c
+++ Rafael/Aula 7/somatorio/main.c
@@ -1,56 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int somatorio(unsigned int, unsigned int);
-unsigned int potencias(unsigned int, unsigned int);
-int count;
+unsigned int somatorio(unsigned int, unsigned int); /*alusão à função*/
+int count; /*variável global*/
 
 int main(void){
-    unsigned int i, pn, n, test, result;
+    unsigned int i, n, x, result=0; int test;
 
     do{
+        printf("Valor de x?");
+        test = scanf("%d", &x);
+        scanf("%*[^\n]"); scanf("%*c");
         printf("Valor de n?");
         test = scanf("%d", &n);
         scanf("%*[^\n]"); scanf("%*c");
-    }while(test == 0);
-    do{
-        printf("Valor de n?");
-        test = scanf("%d", &pn);
-        scanf("%*[^\n]"); scanf("%*c");
+
     }while(test == 0);
 
     for(i = 0; i <= n; i++){
         count = 0;
-        result = somatorio(pn, n);
-        printf("Somatório de %2d^2 = %5u e custou %3d operações\n", i, result, count);
+        result += somatorio(x, i);
+        printf("Somatorio %2d^%2d = %5u e custou %3d operações\n", x, i, result, count);
     }
 
     exit(0);
 }
 
 unsigned int somatorio(unsigned int pn, unsigned int n){
-    if(pn == 0){
-        return 0;
-    }
-
-    int i, sum = 0;
-    for(i = 0; i<=pn; i++){
-        sum += potencias(pn, i);
-    }
-
-    return sum;
-}
-
-unsigned int potencias(unsigned int pn, unsigned int n){
-    if(n==0){
+    int som = 0;
+    if(n == 0){
         return 1;
-    }else if(n==1){
+    }else if(n == 1){
         return pn;
-    }else if(n%2==0){
+    }else if(n%2 == 0){
         count += 2;
-        return potencias(pn*pn, n/2);
+        som += somatorio(pn*pn, n/2);
     }else{
         count += 3;
-        return pn * potencias(pn*pn, n/2);
+        som = som + (pn * (somatorio(pn*pn, n/2)));
     }
+
+    return som;
 }
+
